226a344b1538ef8db505415d6788dfe18e8ecb51
Hag-Zilla <d.mascheix@gmail.com>
2022-10-21 12:10:31 +0000

    delete base url

--- test.py
+++ test.py
@@ -1,16 +1,16 @@
 from docker import DockerClient
 
-client = DockerClient(base_url= 'unix://var/run/docker.sock')
+client = DockerClient()
 print("coucou")
-# client.containers.run(image='datascientest/neo4j:latest',
-#                       name='my_neo4j',
-#                       detach=True,
-#                       auto_remove=True,
-#                       ports={
-#                           '7474/tcp': 7474,
-#                            '7687/tcp':7687},
-#                       network='bridge'
-#                     )
+client.containers.run(image='datascientest/neo4j:latest',
+                      name='my_neo4j',
+                      detach=True,
+                      auto_remove=True,
+                      ports={
+                          '7474/tcp': 7474,
+                           '7687/tcp':7687},
+                      network='bridge'
+                    )
 
 # printing names of active containers
 for c in client.containers.list():
